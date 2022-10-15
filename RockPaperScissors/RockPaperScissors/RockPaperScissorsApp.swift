da7918d077d0242d84cb9357602c1a1825da52c1
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-15 23:04:39 +0400

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Abdurokhman Mukhanbetov on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
